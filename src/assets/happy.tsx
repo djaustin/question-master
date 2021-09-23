1144cad1caa9608a2e0a00b38fbb459e479165b6
Toby Dickinson Williams <toby.jay.dickinson@gmail.com>
2021-09-23 16:17:17 +0100

    feat: Create likert scale component

---
+++ src/assets/happy.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useColorModeValue } from "@chakra-ui/color-mode";
+
+const HappySvg = props => (
+<svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="48" cy="48" r="44" stroke={useColorModeValue("black", "white")} stroke-width="8"/>
+<path d="M29 59C45 77.111 61.6667 66.5462 68 59" stroke={useColorModeValue("black", "white")} stroke-width="8"/>
+<circle cx="34.5" cy="32.5" r="6.5" fill={useColorModeValue("black", "white")}/>
+<circle cx="61.5" cy="32.5" r="6.5" fill={useColorModeValue("black", "white")}/>
+</svg>
+
+);
+
+export default HappySvg;
--- src/components/FeedbackButton.tsx
+++ src/components/FeedbackButton.tsx
@@ -1,21 +1,46 @@
 import { useColorModeValue } from "@chakra-ui/color-mode";
 import { Box } from "@chakra-ui/layout";
-import Image from "next/image";
-import React from "react";
-import frown from "../assets/frown.svg";
+import React, { ReactElement } from "react";
+import FrownSvg from "../assets/frown";
+import SadSvg from "../assets/sad";
+import GrinSvg from "../assets/grin";
+import NeutralSvg from "../assets/neutral";
+import HappySvg from "../assets/happy";
 
-export default function FeedbackButton() {
+
+export type FeedbackButtonProps = {
+  variant: 'frown' | 'sad' | 'neutral' | 'happy' | 'grin'
+}
+
+export default function FeedbackButton({variant}: FeedbackButtonProps ) {
+  let svg: ReactElement;
+  switch(variant){ 
+    case "frown":
+      svg = <FrownSvg/>;
+      break;
+    case "sad": 
+      svg = <SadSvg/>;
+      break;
+    case 'neutral':
+      svg = <NeutralSvg/>;
+      break;
+    case 'happy': 
+      svg = <HappySvg/>;
+      break;
+    case 'grin':
+      svg = <GrinSvg/>;
+      break;
+  }
+  
   return (
     <Box
-      h="50px"
-      w="50px"
       p="1"
       color="red"
       textColor="red"
       borderRadius="lg"
       _hover={{ bg: useColorModeValue("gray.100", "gray.700") }}
     >
-      <Image src={frown} />
+      {svg}
     </Box>
   );
 }
