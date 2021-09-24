20fa24967aa11935574ad22c3357fc09ad202216
Dan Austin <dan.austin@hey.com>
2021-09-24 13:59:10 +0100

    feat: make faces into radio group and add form
    Co-authored-by: annabelkimber <annabelkimber@users.noreply.github.com>
    Co-authored-by: tigs1995 <tigs1995@users.noreply.github.com>

---
+++ src/components/LikertScale.tsx
@@ -0,0 +1,42 @@
+import { Stack, StackProps, useRadioGroup } from "@chakra-ui/react";
+import React from "react";
+import { ControllerRenderProps } from "react-hook-form";
+import FeedbackButton, { FeedbackButtonProps } from "./FeedbackButton";
+
+const responses: { variant: FeedbackButtonProps["variant"]; value: string }[] =
+  [
+    { variant: "sad", value: "1" },
+    { variant: "frown", value: "2" },
+    { variant: "neutral", value: "3" },
+    { variant: "happy", value: "4" },
+    { variant: "grin", value: "5" },
+  ];
+
+export type LikertScaleProps = {
+  fieldProps?: ControllerRenderProps;
+} & StackProps;
+
+const LikertScale: React.FC<LikertScaleProps> = ({
+  fieldProps,
+  ...stackProps
+}) => {
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: "score",
+    onChange: console.log,
+  });
+  return (
+    <Stack
+      {...stackProps}
+      {...getRootProps()}
+      {...fieldProps}
+      direction={{ base: "column", sm: "row" }}
+    >
+      {responses.map(({ variant, value }) => {
+        const radioProps = getRadioProps({ value });
+        return <FeedbackButton key={value} variant={variant} {...radioProps} />;
+      })}
+    </Stack>
+  );
+};
+
+export default LikertScale;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,38 @@
-import { Container, Center, Heading, VStack, Button, useColorMode } from "@chakra-ui/react";
+import {
+  Container,
+  Center,
+  Heading,
+  VStack,
+  Button,
+  useColorMode,
+} from "@chakra-ui/react";
 import React from "react";
-import LikertScale from "../components/likertScale";
+import { Controller, useForm } from "react-hook-form";
+import LikertScale from "../components/LikertScale";
+
+type FormInputs = {
+  score: string;
+};
 
 const Index = () => {
-const {toggleColorMode} = useColorMode();
- return (
-  <Container maxW='4xl'>
-    <VStack>
-      <Heading size="lg" >How are you finding the systems performance today?</Heading>
-      <LikertScale/>
-      <Button
-        onClick={async () =>
-          await fetch("/api/feedback", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ score: 2 }),
-          })
-        }
-      >
-        Submit
-      </Button>
-      <Button
-        onClick={toggleColorMode
-        }
-      >
-        Submit
-      </Button>
-    </VStack>
-  </Container>
- )
+  const { handleSubmit, control } = useForm<FormInputs>();
+  return (
+    <Container maxW="4xl">
+      <VStack>
+        <Heading size="lg">
+          How are you finding the systems performance today?
+        </Heading>
+        <form onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}>
+          <Controller
+            name="score"
+            render={({ field }) => <LikertScale fieldProps={field} />}
+            control={control}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </VStack>
+    </Container>
+  );
 };
 
 export default Index;
