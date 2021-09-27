5cb4ed04df2dfcdcf5a0da047bc8e65d340fad9c
Dan Austin <dan.austin@hey.com>
2021-09-27 17:41:59 +0100

    fix: prevent form submit when no score selected
    * Adds Cypress tests

--- src/assets/neutral.tsx
+++ src/assets/neutral.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React from "react";
 import { useColorModeValue } from "@chakra-ui/color-mode";
 
-const NeutralSvg = props => (
-<svg width="96" height="96" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="48" cy="48" r="44" stroke={useColorModeValue("black", "white")} stroke-width="8"/>
-<path d="M27 64C44.2308 64 62.1795 64 69 64" stroke={useColorModeValue("black", "white")} stroke-width="8"/>
-<circle cx="34.5" cy="32.5" r="6.5" fill={useColorModeValue("black", "white")}/>
-<circle cx="61.5" cy="32.5" r="6.5" fill={useColorModeValue("black", "white")}/>
-</svg>
+const NeutralSvg = (props) => (
+  <svg
+    aria-label="neutral"
+    width="96"
+    height="96"
+    viewBox="0 0 96 96"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="48"
+      cy="48"
+      r="44"
+      stroke={useColorModeValue("black", "white")}
+      stroke-width="8"
+    />
+    <path
+      d="M27 64C44.2308 64 62.1795 64 69 64"
+      stroke={useColorModeValue("black", "white")}
+      stroke-width="8"
+    />
+    <circle
+      cx="34.5"
+      cy="32.5"
+      r="6.5"
+      fill={useColorModeValue("black", "white")}
+    />
+    <circle
+      cx="61.5"
+      cy="32.5"
+      r="6.5"
+      fill={useColorModeValue("black", "white")}
+    />
+  </svg>
 );
 
 export default NeutralSvg;
