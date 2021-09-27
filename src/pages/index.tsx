e1c6206b5f9aff4d54a0aafbded16907a0211a7c
Dan Austin <dan.austin@hey.com>
2021-09-27 15:24:45 +0100

    feat: add personal identifier to feedback form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,31 @@
 import {
-  Box,
   Button,
+  Checkbox,
   Container,
   Flex,
+  FormControl,
+  FormErrorMessage,
   Heading,
+  Input,
   VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import LikertScale from "../components/LikertScale";
 
 type FormInputs = {
   score: string;
+  username: string;
 };
 
 const Index = () => {
-  const { handleSubmit, control } = useForm<FormInputs>();
+  const {
+    handleSubmit,
+    control,
+    register,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  const [contactUser, setContactUser] = useState(false);
   const sendFeedback: SubmitHandler<FormInputs> = (data) => {
     const parsedData = { ...data, score: parseInt(data.score) };
     fetch("/api/feedback", {
@@ -31,11 +41,40 @@ const Index = () => {
           How are you finding the system's performance today?
         </Heading>
         <form onSubmit={handleSubmit(sendFeedback)}>
-          <Controller
-            name="score"
-            render={({ field }) => <LikertScale fieldProps={field} />}
-            control={control}
-          />
+          <FormControl isInvalid={!!errors.score}>
+            <Controller
+              name="score"
+              render={({ field }) => <LikertScale fieldProps={field} />}
+              control={control}
+              rules={{
+                required: "Please choose a score before submitting",
+              }}
+            />
+            <FormErrorMessage>{errors.score?.message}</FormErrorMessage>
+          </FormControl>
+          <Checkbox
+            mt="6"
+            checked={contactUser}
+            onChange={(e) => setContactUser(e.target.checked)}
+          >
+            I would like to be contacted about my issue
+          </Checkbox>
+          <FormControl
+            hidden={!contactUser}
+            mt="4"
+            isInvalid={!!errors.username}
+          >
+            <Input
+              placeholder="Username (e.g. az999999)"
+              {...register("username", {
+                required: {
+                  value: contactUser,
+                  message: "Please enter a username to be contacted",
+                },
+              })}
+            />
+            <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
+          </FormControl>
           <Flex mt="8" justify="flex-end">
             <Button size="lg" type="submit">
               Submit
