d9bff83e3d0906f38880b169d80ad9d93c75b6d1
Dan Austin <dan.austin@hey.com>
2021-09-27 11:08:29 +0100

    refactor: move prisma client to own module

--- src/controllers/createFeedback.ts
+++ src/controllers/createFeedback.ts
@@ -1,6 +1,5 @@
-import { Prisma, PrismaClient } from ".prisma/client";
-
-const prisma = new PrismaClient();
+import { Prisma } from "@prisma/client";
+import prisma from "../integrations/db";
 
 export async function createFeedback(data: Prisma.FeedbackCreateInput) {
   return await prisma.feedback.create({ data });
