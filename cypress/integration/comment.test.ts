4e17dc6703e477ae033f6ee8a5102a4592269abd
Dan Austin <dan.austin@hey.com>
2021-09-28 13:44:42 +0100

    test: add username field tests

--- cypress/integration/comment.test.ts
+++ cypress/integration/comment.test.ts
@@ -1,29 +1,43 @@
 describe("Comment Submission", () => {
   it("should submit the user's chosen comment", () => {
+    // Arrange
     cy.intercept("POST", "/api/feedback", {}).as("feedback");
+
+    // Act
     cy.visit("/");
     cy.findByPlaceholderText("Comment").type("Test comment 1");
     cy.findByLabelText("neutral").click();
     cy.findAllByRole("button", { name: /submit/i }).click();
 
-    cy.wait("@feedback").its("request.body.comment").should("eq", "Test comment 1");
+    // Assert
+    cy.wait("@feedback")
+      .its("request.body.comment")
+      .should("eq", "Test comment 1");
   });
 
   it("should submit without a comment", () => {
+    // Arrange
     cy.intercept("POST", "/api/feedback", {}).as("feedback");
+
+    // Act
     cy.visit("/");
     cy.findByLabelText("neutral").click();
     cy.findAllByRole("button", { name: /submit/i }).click();
 
+    // Assert
     cy.wait("@feedback").its("request.body.comment").should("eq", "");
   });
 
   it("should show an error message if you submit without a comment when the 'very unhappy' face is selected", () => {
+    // Arrange
     cy.intercept("POST", "/api/feedback", {}).as("feedback");
+
+    // Act
     cy.visit("/");
     cy.findByLabelText("very unhappy").click();
     cy.findAllByRole("button", { name: /submit/i }).click();
 
+    // Assert
     cy.findByText("Please enter a comment before submitting");
     cy.get("@feedback.all").should("have.length", 0);
   });
--- cypress/integration/score.test.ts
+++ cypress/integration/score.test.ts
@@ -1,18 +1,26 @@
 describe("Score Submission", () => {
   it("should submit the user's chosen score", () => {
+    // Arrange
     cy.intercept("POST", "/api/feedback", {}).as("feedback");
+
+    // Act
     cy.visit("/");
     cy.findByLabelText("neutral").click();
     cy.findAllByRole("button", { name: /submit/i }).click();
 
+    // Assert
     cy.wait("@feedback").its("request.body.score").should("eq", 3);
   });
 
   it("should not submit the form when no score is chosen", () => {
+    // Arrange
     cy.intercept("POST", "/api/feedback").as("feedback");
+
+    // Act
     cy.visit("/");
     cy.findAllByRole("button", { name: /submit/i }).click();
 
+    // Assert
     cy.get("@feedback.all").should("have.length", 0);
     cy.findByText("Please choose a response before submitting");
   });
