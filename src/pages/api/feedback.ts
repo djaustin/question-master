f6990ce6d50ac2df78a33ddbd7f8e4f61e889972
Annabel Kimber <annabelkimber@hotmail.co.uk>
2021-09-30 16:39:22 +0100

    feat: 24hr filter

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -4,7 +4,15 @@ import { createFeedback } from "../../controllers/createFeedback";
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === "POST") return res.json(await createFeedback(req.body));
-  if (req.method === "GET") return res.json(await prisma.feedback.findMany())
+  if (req.method === "GET") return res.json(await prisma.feedback.findMany({
+    where: {
+      createdAt:{
+        gte: new Date(
+          ''
+        )
+      }
+    }
+  }))
 };
 
 export default handler;
