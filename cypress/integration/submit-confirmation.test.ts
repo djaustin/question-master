d63e1f4333a7dcffbe354a8a982025affcef0a4f
Dan Austin <dan.austin@hey.com>
2021-10-01 16:27:40 +0100

    test: rename file to test file

--- cypress/integration/submit-confirmation.test.ts
+++ cypress/integration/submit-confirmation.test.ts
@@ -2,7 +2,8 @@ describe("Submit Confirmation", () => {
   it("should show a success message when submission is successful", () => {
     cy.intercept("POST", "/api/feedback", {}).as("submission");
     cy.visit("/");
-    cy.findByLabelText(/neutral/i).click();
+
+    cy.findByLabelText("neutral").click();
     cy.findByRole("button", { name: /submit/i }).click();
     cy.findByText(/feedback has been submitted/i);
   });
@@ -10,7 +11,8 @@ describe("Submit Confirmation", () => {
   it("should show an error message when submission is unsuccessful", () => {
     cy.intercept("POST", "/api/feedback", { statusCode: 500 }).as("submission");
     cy.visit("/");
-    cy.findByLabelText(/neutral/i).click();
+
+    cy.findByLabelText("neutral").click();
     cy.findByRole("button", { name: /submit/i }).click();
     cy.findByText(/there was an error submitting/i);
   });
