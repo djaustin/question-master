e31feaa37fed4acaa47882aacaff0a2111645108
Dan Austin <dan.austin@hey.com>
2021-10-01 15:15:04 +0100

    feat: add post config endpoint and schema migrations

---
+++ prisma/migrations/20211001140155_config/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Config" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "Config_pkey" PRIMARY KEY ("id")
+);
---
+++ src/pages/api/config.ts
@@ -0,0 +1,23 @@
+import { Config } from "@prisma/client";
+import { NextApiHandler } from "next";
+import prisma from "../../integrations/db";
+
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method === "POST") {
+    req.body.forEach(
+      async (config: Config) =>
+        await prisma.config.upsert({
+          where: { key: config.key.toLowerCase() },
+          create: config,
+          update: {
+            value: config.value,
+          },
+        })
+    );
+    res.status(200);
+    return res.end();
+  }
+  if (req.method === "GET") return res.json(await prisma.config.findMany());
+};
+
+export default handler;
