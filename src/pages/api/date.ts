788d25a79983774f827c83b7a3eca7a8dbe098a9
Tigs Knowles <tigsknowles@Tigss-MacBook-Air.local>
2021-10-07 11:24:55 +0100

    feat: Add test for date picker

--- src/pages/api/date.ts
+++ src/pages/api/date.ts
@@ -2,21 +2,21 @@ import prisma from "../../integrations/db";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
-  const startDate = req.body[0];
-  const endDate = req.body[0];
+  const dateRange = req.query.dateRange as string;
+  const dateRangeArray = dateRange.split(",");
 
-  if (req.method === "POST"){
+  if (req.method === "GET"){
     return res.json(await prisma.feedback.findMany({
       where: {
         AND: [
           {
             createdAt: {
-              gte: startDate
+              gte: dateRangeArray[0]
             },
           },
           {
             createdAt: {
-              lte: endDate
+              lte: dateRangeArray[1]
             },
           }
         ]}
