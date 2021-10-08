ed1517f2a8b956d058078a22da21d5361221879b
Dan Austin <dan.austin@hey.com>
2021-10-08 10:31:28 +0100

    Feat/date filter da (#17)
    * fix: prevent whole page rerender on date change
    
    * refactor: make DatePicker more generic by moving url change logic to parent
    
    * feat: change spinner to skeleton components
    
    * feat: make datepicker clearable
    
    * refactor: organize imports and rename date picker test file
    
    * refactor: move date filter logic into feedback endpoint
    
    * test: add cypress test for date filter
    
    * feat: Remove date api page
    
    Co-authored-by: Tigs Knowles <tigsknowles@Tigss-MacBook-Air.local>

--- src/controllers/createFeedback.ts
+++ src/controllers/createFeedback.ts
@@ -1,6 +1,32 @@
 import { Prisma } from "@prisma/client";
+import { NextApiRequest } from "next";
 import prisma from "../integrations/db";
 
 export async function createFeedback(data: Prisma.FeedbackCreateInput) {
   return await prisma.feedback.create({ data });
+}
+
+export async function getFeedback(query: NextApiRequest["query"]) {
+  const dateRange = query.dateRange as string;
+
+  if (!dateRange) return prisma.feedback.findMany();
+
+  const dateRangeArray = dateRange?.split(",");
+
+  return prisma.feedback.findMany({
+    where: {
+      AND: [
+        {
+          createdAt: {
+            gte: dateRangeArray?.[0],
+          },
+        },
+        {
+          createdAt: {
+            lte: dateRangeArray?.[1],
+          },
+        },
+      ],
+    },
+  });
 }
