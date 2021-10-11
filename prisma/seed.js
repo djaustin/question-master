1a9013b39161aa195442ee633818022972f181ef
Dan Austin <dan.austin@hey.com>
2021-10-11 17:36:55 +0100

    fix: add device to feedback response

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,17 +5,32 @@ const prisma = new Prisma.PrismaClient();
 const seed = async () => {
   let data = [];
   for (let index = 0; index < 100; index++) {
+    const randomIP = faker.internet.ip();
     const feedback = {
       comment: faker.lorem.sentence(),
       createdAt: faker.date.recent(5),
       updatedAt: faker.date.recent(3),
       score: faker.datatype.number(4) + 1,
       username: faker.internet.userName(),
+      device: {
+        connectOrCreate: {
+          create: {
+            ip: randomIP,
+            hostname: faker.internet.domainName(),
+            location: faker.address.city(),
+          },
+          where: {
+            ip: randomIP,
+          },
+        },
+      },
     };
     data.push(feedback);
   }
   for (const item of data) {
-    await prisma.feedback.create({ data: item });
+    await prisma.feedback.create({
+      data: item,
+    });
   }
 };
 
