ce4e19e73708b6711e5d17c3ab9142b840c58447
Dan Austin <dan.austin@hey.com>
2021-10-12 17:29:55 +0100

    feat: add large scorecard variant for summary and wallboard

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,12 +1,41 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Center, Text, Theme } from "@chakra-ui/react";
 import { Response } from "../models/response";
 import React from "react";
+import { LayoutProps } from "framer-motion";
 
 export type ScoreCardProps = {
   variant: Response;
+  size?: "md" | "lg";
 };
 
-export const ScoreCard: React.FC<ScoreCardProps> = ({ variant, children }) => {
+export const ScoreCard: React.FC<ScoreCardProps> = ({
+  variant,
+  size = "md",
+  children,
+}) => {
+  let { textBackgroundColor, textColor } = colorVariants(variant);
+  let { fontSize, boxSize } = sizeVariants(size);
+  return (
+    <Center
+      h={boxSize}
+      w={boxSize}
+      justifyContent="center"
+      bg={textBackgroundColor}
+      px={size === "md" && 2}
+      borderRadius="md"
+    >
+      <Text
+        verticalAlign="center"
+        fontSize={fontSize}
+        fontWeight="black"
+        color={textColor}
+      >
+        {children}
+      </Text>
+    </Center>
+  );
+};
+function colorVariants(variant: string) {
   let textBackgroundColor: string,
     textColor: string = "white";
   switch (variant) {
@@ -28,16 +57,16 @@ export const ScoreCard: React.FC<ScoreCardProps> = ({ variant, children }) => {
       textBackgroundColor = "#51bf03";
       break;
   }
-  return (
-    <Box
-      justifyContent="center"
-      bg={textBackgroundColor}
-      px="2"
-      borderRadius="md"
-    >
-      <Text fontWeight="black" textAlign="center" color={textColor}>
-        {children}
-      </Text>
-    </Box>
-  );
-};
+  return { textBackgroundColor, textColor };
+}
+function sizeVariants(size: "md" | "lg") {
+  let fontSize: string, boxSize: string;
+  switch (size) {
+    case "lg":
+      fontSize = "5xl";
+      boxSize = "96px";
+    case "md":
+      break;
+  }
+  return { fontSize, boxSize };
+}
