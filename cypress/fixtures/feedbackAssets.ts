f42adfbf29787319656fcf3dbf538214e3ac791f
tigs1995 <tigsye@hotmail.com>
2021-10-15 12:24:36 +0100

    feat: Use dayjs to convert date picker text for cypress test

--- cypress/fixtures/feedbackAssets.ts
+++ cypress/fixtures/feedbackAssets.ts
@@ -101,6 +101,6 @@ export default function setToStartOfDay(date: Date){
   date.setMilliseconds(0);
 };
 
-const defaultDatePickerText = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`;
+const defaultDatePickerText = `${dayjs(new Date).format("DD/MM/YYYY")}`;
 
 export { feedbackData, defaultDatePickerText };
