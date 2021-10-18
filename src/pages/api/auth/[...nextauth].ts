01cb560b7eaad326fbc98a2355baf7af0aa1e597
tigs.knowles <tigsye@hotmail.com>
2021-10-18 14:08:27 +0100

    feat: add ldap authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import Providers from "next-auth/providers";
+import {authenticate} from 'ldap-authentication'
 
 const options: NextAuthOptions = {
   providers: [
@@ -17,6 +18,25 @@ const options: NextAuthOptions = {
           password === process.env.ADMIN_PASSWORD
         )
           return { name: "Local Administrator" };
+
+        const ldapAuthConfig = {
+          ldapOpts: {
+            url: process.env.LDAP_URL
+          },
+          adminDn: process.env.LDAP_ADMIN_DN,
+          adminPassword: process.env.LDAP_ADMIN_PASSWORD,
+          username,
+          userPassword: password,
+          userSearchBase: process.env.LDAP_USER_SEARCH_BASE,
+          usernameAttribute: process.env.LDAP_USERNAME_ATTRIBUTE
+        }
+        try {
+          const user = await authenticate(ldapAuthConfig)
+          return {...user, name: user.displayName || user.gecos}
+        } catch (err) {
+          console.log(err)
+          return null;
+        }
       },
     }),
   ],
