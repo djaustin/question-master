2ecd34bd478ca691b632c473fb9cc1518695252e
tigs1995 <tigsye@hotmail.com>
2021-10-21 10:37:16 +0100

    fix: Fix failing tests

--- cypress/integration/dashboard.test.ts
+++ cypress/integration/dashboard.test.ts
@@ -21,26 +21,36 @@ describe("Dashboard", () => {
       fromDate = dayjs(fromDate).startOf("day").toDate();
       toDate = dayjs(toDate).endOf("day").toDate();
 
-      const expectedQuery = [fromDate, toDate]
+      cy.intercept("GET", "/api/feedback*", []).as("feedback");
+      const expectedQuery: string = [fromDate, toDate]
         .map((date) => date.toISOString())
         .join(",");
+
+      const expectedQueryReplaceColons = expectedQuery.replaceAll(":", "%3A");
+      const expectedQueryReplaceCommas = expectedQueryReplaceColons.replaceAll(",", "%2C");
+
       const fromDateRegex = new RegExp(`\\s+${fromDate.getDate()}th`);
       const toDateRegex = new RegExp(`\\s+${toDate.getDate()}rd`);
 
       // Act
       cy.intercept("GET", "/api/feedback*", []).as("feedback");
       cy.visit("/dashboard/results");
-      cy.wait('@feedback');
-      cy.wait(500);
 
-      cy.get(`input[value="${defaultDatePickerText}"]`).click();
+      // Assert
+      cy.wait("@feedback");
+      cy.wait("@feedback").its("request.url").should("include", "skip=0");
+
+      // Act
+      cy.findAllByRole("textbox").eq(0).click();
       cy.findAllByRole("button", { name: fromDateRegex }).click();
       cy.findAllByRole("button", { name: toDateRegex }).click();
       cy.intercept("GET", "/api/feedback*", []).as("feedback");
 
+      cy.intercept("GET", "/api/feedback*", []).as("feedback");
+
       // Assert
       cy.wait("@feedback");
-      cy.wait("@feedback").its("request.url").should("include", expectedQuery);
+      cy.wait("@feedback").its("request.url").should("include", expectedQueryReplaceCommas);
     });
     it("should display a table of results", () => {
       cy.visit("/dashboard/results");
